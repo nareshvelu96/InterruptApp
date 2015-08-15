9fcbf48b41c06cbf297d4d4a37a130cb9f9ab8f7
madithya2509 <madithya2509@gmail.com>
2015-08-15 19:16:42 +0530

    stable v1

--- app/src/main/java/com/example/root/interrupt/SVCEMapFragment.java
+++ app/src/main/java/com/example/root/interrupt/SVCEMapFragment.java
@@ -1,11 +1,24 @@
 package com.example.root.interrupt;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by root on 9/8/15.
  */
 public class SVCEMapFragment extends Fragment {
 
+    View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+        rootView = inflater.inflate(R.layout.mapfragment, container, false);
+        return rootView;
+    }
 
 }
